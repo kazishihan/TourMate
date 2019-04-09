09559332ef24baa0ab272c6040868c45c1662f74
kazishihan <kazishihan@gmail.com>
2019-04-09 23:58:00 +0600

    add memory bottomsheet and floating action button

--- app/src/main/java/com/kazishihan/tourmate/Fragment/MemoryFragment.java
+++ app/src/main/java/com/kazishihan/tourmate/Fragment/MemoryFragment.java
@@ -2,11 +2,13 @@ package com.kazishihan.tourmate.Fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kazishihan.tourmate.BottomSheet_AddMemory;
 import com.kazishihan.tourmate.BottomSheet_AddTrip;
 import com.kazishihan.tourmate.R;
 
@@ -14,6 +16,9 @@ import com.kazishihan.tourmate.R;
  * A simple {@link Fragment} subclass.
  */
 public class MemoryFragment extends Fragment {
+
+    private FloatingActionButton floatingActionButtonMemory;
+    private BottomSheet_AddMemory bottomSheet_addMemory;
 
 
     public MemoryFragment() {
@@ -25,9 +30,23 @@ public class MemoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_memory, container, false);
+         View view=inflater.inflate(R.layout.fragment_memory, container, false);
+
+         floatingActionButtonMemory = view.findViewById(R.id.fabMemory);
 
 
+         floatingActionButtonMemory.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+                 bottomSheet_addMemory = new BottomSheet_AddMemory();
+                 bottomSheet_addMemory.show(getFragmentManager(),"BottomSheet_addmemory");
+                // .show(getFragmentManager(), "BootmSheet_addtrip");
+             }
+         });
+
+
+         return view;
     }
 
 }
